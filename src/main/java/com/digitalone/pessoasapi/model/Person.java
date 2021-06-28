1265c4cf83989d2d3c5488f10b85ad90d2c219d1
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-28 17:16:18 -0300

    Mapeamento das Classes Efetuado.

--- src/main/java/com/digitalone/pessoasapi/model/Person.java
+++ src/main/java/com/digitalone/pessoasapi/model/Person.java
@@ -4,7 +4,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,16 +25,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Person {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nome;
 	
+	@Column(nullable = false)
 	private String sobrenome;
 	
+	@Column(nullable = false, unique = true)
 	private String cpf;
 	
 	private LocalDate dataNascimento;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
 	private List<Phone> telefone = new ArrayList<>();
 
 }
