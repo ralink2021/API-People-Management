9c84a3f6998dbd6e4ebdb39d2e78a73a7ed25c56
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-28 14:15:35 -0300

    Inicio do Projeto

---
+++ src/main/java/com/digitalone/pessoasapi/PessoasapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalone.pessoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasapiApplication.class, args);
+	}
+
+}
